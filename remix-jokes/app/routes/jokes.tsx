c9a58cdb8bda26e3a490eae617b0b158ae5677d5
chiconasa3000 <csucavelando@gmail.com>
2023-10-09 15:54:06 -0500

    styles and routes in remix

---
+++ remix-jokes/app/routes/jokes.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "@remix-run/react";
+export default function JokesRoute(){
+    return(
+        <div>
+            <h1>Jokes 🧐</h1>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    );
+}
